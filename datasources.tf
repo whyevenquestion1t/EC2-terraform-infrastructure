5509f31bf0ff06ba349c343fe2162bd2a7687102
whyevenquestion1t <shoysoronovr@gmail.com>
2023-03-24 14:41:53 -0400

    added an instance and user data

--- datasources.tf
+++ datasources.tf
@@ -1,9 +1,19 @@
-resource "aws_ami" "terraform_ami" {
-    name = "terraform_example"
-    owners = ["193190103167"]
+data "aws_ami" "terraform_ami" {
 
-    filter {
-        name = "name"
-        values = [" amazon/ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-arm64-server-*"]
-    }
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"] #["ami-0263e4deb427da90e"]
+  }
 }
+
+# data "aws_ami_ids" "ubuntu" {
+#   owners = ["099720109477"]
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/ubuntu-*-*-amd64-server-*", "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20230131"]
+#   }
+# }
--- main.tf
+++ main.tf
@@ -70,23 +70,26 @@ resource "aws_security_group" "mtc_sg" {
 }
 
 resource "aws_key_pair" "mtc_auth" {
-    key_name = "deployer-key"
-    public_key = file("~/.ssh/mtckey.pub")
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/mtckey.pub")
 }
 
 resource "aws_instance" "dev_node" {
-    instance_type = "t2.micro"
-    ami = aws_ami.server_ami.id
 
-    tags = {
-        Name = "dev_node"
-    }
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.terraform_ami.id #data.aws_ami_ids.ubuntu.id
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+  user_data = file("userdata.tpl")
 
-    key_name = aws_key_pair.mtc_auth.id
-    vpc_security_group_ids = [aws_security_group.mtc_sg.id]
-    subnet_id = aws_subnet.mtc_public_subnet.id
+  root_block_device {
+    volume_size = 8
+  }
+  tags = {
+    Name = "dev_node"
+  }
 
-    root_block_device {
-        volume_size = 8
-    }
+  
+
 }
